be76be4202d520c836993a926cd22c6bc43100b9
shankkyy <shivv2403@gmail.com>
2024-05-26 21:55:19 +0530

    resume link change

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,94 @@
+import React from "react";
+import {
+  Flex,
+  Link,
+  List,
+  ListItem,
+  Text,
+  useColorModeValue
+} from "@chakra-ui/react";
+import { AiFillGithub, AiFillHeart } from "react-icons/ai";
+import { BiLogoLinkedin } from "react-icons/bi";
+import { SiLeetcode } from "react-icons/si";
+
+export default function Footer() {
+  const textColor = useColorModeValue("lightblue.100", "lightpeach.100");
+
+  return (
+    <Flex
+      zIndex="3"
+      flexDirection={{
+        base: "column",
+        xl: "row"
+      }}
+      alignItems={{
+        base: "center",
+        xl: "start"
+      }}
+      justifyContent="space-between"
+      px={{ base: "30px", md: "40px" }}
+      pb="30px"
+      mt="20px"
+    >
+      <Text
+        color={textColor}
+        textAlign={{
+          base: "center",
+          xl: "start"
+        }}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        fontSize={{
+          base: "10px",
+          md: "15px",
+          xl: "20px"
+        }}
+        mb={{ base: "20px", xl: "0px" }}
+      >
+        {" "}
+        &copy; {1900 + new Date().getYear()}
+        {"</>"} with    <AiFillHeart />  by Nishank • All Rights Reserved
+      </Text>
+      <List display="flex">
+        <ListItem
+          me={{
+            base: "20px",
+            md: "40px"
+          }}
+          opacity="0.6"
+          _hover={{ opacity: 1 }}
+        >
+          <Link href="https://github.com/shankkyy" target="blank">
+            <AiFillGithub />
+          </Link>
+        </ListItem>
+        <ListItem
+          me={{
+            base: "20px",
+            md: "40px"
+          }}
+          opacity="0.6"
+          _hover={{ opacity: 1 }}
+        >
+          <Link href="https://leetcode.com/u/nishank24/" target="blank">
+            <SiLeetcode />
+          </Link>
+        </ListItem>
+        <ListItem
+          me={{
+            base: "20px",
+            md: "40px"
+          }}
+          opacity="0.6"
+          _hover={{ opacity: 1 }}
+        >
+          <Link href="https://www.linkedin.com/in/nishank-verma/" target="blank">
+            <BiLogoLinkedin />
+          </Link>
+        </ListItem>
+       
+      </List>
+    </Flex>
+  );
+}
